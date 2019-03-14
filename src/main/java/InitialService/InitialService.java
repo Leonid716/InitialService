44ce386e4ef26c448d41ef3e607af64bec2253b0
student <student@localhost>
2019-03-14 21:31:21 +0300

    Fixed #3, Fixed #4

--- src/main/java/InitialService/InitialService.java
+++ src/main/java/InitialService/InitialService.java
@@ -5,18 +5,17 @@ public class InitialService {
         String parts[] = name.split(" ");
 
         if (parts.length == 1) {
-            return parts[0].substring(0, 1);
+            return parts[0].substring(0, 1).toUpperCase();
+        }
+        if (parts.length == 2) {
+            return parts[0].substring(0, 1).toUpperCase() + parts[1].substring(0, 1).toUpperCase();
         }
 
         if (parts.length == 3) {
-            return parts[0].substring(0, 1) + parts[2].substring(0, 1);
+            return parts[0].substring(0, 1).toUpperCase() + parts[2].substring(0, 1).toUpperCase();
         }
 
-        String firstLetter = parts[0].substring(0, 1);
-        String secondLetter = parts[1].substring(0, 1);
-
-        return firstLetter.toUpperCase() + secondLetter.toUpperCase();
-
+        return null;
     }
 }
 
