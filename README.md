a0435529287ac8c9db7bc40cc30eec4c5ec58b43
student <student@localhost>
2019-03-14 22:10:14 +0300

    Fixed #2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-[![Build Status](https://travis-ci.com/Leonid716/InitialService.svg?branch=master)](https://travis-ci.com/Leonid716/InitialService) 
+[![Build Status](https://travis-ci.com/Leonid716/InitialService.svg?branch=master)](https://travis-ci.com/Leonid716/InitialService)  [![codecov](https://codecov.io/gh/Leonid716/InitialService/branch/master/graph/badge.svg)](https://codecov.io/gh/Leonid716/InitialService)
 
 
 # Initial service
--- src/main/java/InitialService/InitialService.java
+++ src/main/java/InitialService/InitialService.java
@@ -7,15 +7,15 @@ public class InitialService {
         if (parts.length == 1) {
             return parts[0].substring(0, 1).toUpperCase();
         }
-        if (parts.length == 2) {
-            return parts[0].substring(0, 1).toUpperCase() + parts[1].substring(0, 1).toUpperCase();
-        }
 
         if (parts.length == 3) {
             return parts[0].substring(0, 1).toUpperCase() + parts[2].substring(0, 1).toUpperCase();
         }
+        String firstLetter = parts[0].substring(0, 1);
+        String secondLetter = parts[1].substring(0, 1);
 
-        return null;
+        return firstLetter.toUpperCase() + secondLetter.toUpperCase();
+
     }
 }
 
