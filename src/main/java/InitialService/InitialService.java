91260a7cd733a18663c7769333f6ab3dd71921e6
student <student@localhost>
2019-03-17 19:52:12 +0300

    Fixed #4

--- src/main/java/InitialService/InitialService.java
+++ src/main/java/InitialService/InitialService.java
@@ -8,14 +8,10 @@ public class InitialService {
             return parts[0].substring(0, 1).toUpperCase();
         }
 
-        if (parts.length == 3) {
-            return parts[0].substring(0, 1).toUpperCase() + parts[2].substring(0, 1).toUpperCase();
+        if (parts.length >= 2) {
+            return parts[0].substring(0,1).toUpperCase() + parts[parts.length - 1].substring(0,1).toUpperCase();
         }
-        String firstLetter = parts[0].substring(0, 1);
-        String secondLetter = parts[1].substring(0, 1);
-
-        return firstLetter.toUpperCase() + secondLetter.toUpperCase();
-
+        return null;
     }
 }
 
