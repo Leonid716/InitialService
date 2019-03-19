063c62ad3d57f687c2d9a0803545f86f730dbbfc
student <student@localhost>
2019-03-20 00:49:49 +0300

    Fixed #4

--- src/main/java/InitialService/InitialService.java
+++ src/main/java/InitialService/InitialService.java
@@ -4,14 +4,11 @@ public class InitialService {
     public String getInitialsOne(String name) {
         String parts[] = name.split(" ");
 
-        if (parts.length == 1) {
-            return parts[0].substring(0, 1).toUpperCase();
-        }
-
         if (parts.length >= 2) {
+            return parts[0].substring(0,1).toUpperCase() + parts[parts.length - 1].substring(0,1).toUpperCase();
 
         }
-        return parts[0].substring(0,1).toUpperCase() + parts[parts.length - 1].substring(0,1).toUpperCase();
+        return parts[0].substring(0, 1).toUpperCase();
     }
 }
 
